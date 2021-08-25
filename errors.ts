2445d34c3205bfafaca7ba6897631129d11f253a
Matthew Smith <matthew.smith@apptreesoftware.com>
2021-08-24 18:22:04 -0700

    Fix error catching

--- errors.ts
+++ errors.ts
@@ -1,49 +1,49 @@
-import {AxiosResponse} from "axios";
-
+import { AxiosResponse } from 'axios';
 
 export class ApiError extends Error {
+  private resp: AxiosResponse;
 
-    private resp : AxiosResponse;
+  constructor(resp: AxiosResponse) {
+    super();
 
-    constructor(resp: AxiosResponse) {
-        super();
+    this.resp = resp;
+    // Set the prototype explicitly.
+    Object.setPrototypeOf(this, ApiError.prototype);
+  }
 
-        this.resp = resp;
-        // Set the prototype explicitly.
-        Object.setPrototypeOf(this, ApiError.prototype);
+  get isAuthorizationError(): boolean {
+    return this.resp.status == 401 || this.resp.status === 403;
+  }
+
+  get message(): string {
+    try {
+      if (this.resp.data.Message) {
+        return this.resp.data.Message;
+      }
+    } catch (e) {}
+    return this.resp.statusText;
+  }
+
+  toString() {
+    if (this.isAuthorizationError) {
+      return 'Authorization Failed';
     }
-
-    get isAuthorizationError() : boolean {
-        return this.resp.status == 401 || this.resp.status === 403;
-    }
-
-    get message() : string {
-        if (this.resp.data.Message) {
-            return this.resp.data.Message;
-        }
-        return this.resp.statusText;
-    }
-
-    toString() {
-        if (this.isAuthorizationError) {
-            return "Authorization Failed";
-        }
-        let str = this.message;
-        if (this.resp.request) {
-            str = `${str} 
+    let str = this.message;
+    if (this.resp.request) {
+      str = `${str} 
 METHOD: ${this.resp.request.method}
 HOST: ${this.resp.config.baseURL}
 URL: ${this.resp.request.path}
-DATA: ${this.resp.config.data}
 `;
-        }
-        return str;
     }
+    return str;
+  }
 }
 
 export const AuthorizationError = new ApiError({
-                                            config: {},
-                                            data: null,
-                                            headers: null,
-                                            statusText: "Authorization Failed",
-                                            status: 401})
+  config: {},
+  data: null,
+  headers: null,
+  statusText: 'Authorization Failed',
+  status: 401,
+});
