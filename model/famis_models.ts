0c4b8cf7665971ea595f053a4a045b89cd7a02fb
eyuapptree <eddie.yu@apptreesoftware.com>
2024-08-02 15:23:25 +0800

    correct data type of asset

--- model/famis_models.ts
+++ model/famis_models.ts
@@ -1455,14 +1455,14 @@ export interface Asset {
   PurchaseDate?: Date;
   PurchaseAmount?: number;
   ExternalCostCenterId?: number;
-  PurchaseCostCenter?: number;
+  PurchaseCostCenter?: string;
   PoNumber?: string;
   EstimatedLifeInYears: number;
   EstimatedLifeInHours: number;
   AnnualRuntimeInHours: number;
   CapitalRepairCost: number;
   EstimatedReplacementCost?: number;
-  LifeExpectancy?: number;
+  LifeExpectancy?: Date;
   UnitMultiplier?: number;
   MeterMultiplier?: number;
   MeterFormat?: number;
@@ -1471,7 +1471,7 @@ export interface Asset {
   LastCalibrationDate?: Date;
   MeterSiteId?: number;
   MeterInstallationDate?: Date;
-  MeterInitialReading?: string;
+  MeterInitialReading?: number;
   MeterLastReadingDate?: Date;
   MeterLastReading?: number;
   UpdateDate: Date;
@@ -1492,6 +1492,7 @@ export interface Asset {
   AssetModel?: AssetModel;
   AssetRank?: AssetRank;
   AssetClass?: AssetClass;
+  AssetMake?: AssetMake;
   ParentAssetAssociation?: ParentAssetAssociation[];
   Attachments?: AssetAttachment[];
 }
